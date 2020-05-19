6b6e92420df75857e673065af5ab1bae4bddbd53
gage117 <57643501+gage117@users.noreply.github.com>
2020-05-19 14:30:52 -0500

    Fixed total correct render

--- src/components/Word/WordList.js
+++ src/components/Word/WordList.js
@@ -5,13 +5,15 @@ import './WordList.css'
 
 export default class WordsList extends React.Component {
     render() {
+        let totalCorrect = 0;
+        this.props.words.map(word => totalCorrect += word.correct_count)
         return(
             <div className='words-list-container'>
                 <h3 className='words-list-header'>Words to practice</h3>
                 <ul className='words-list-ul'>
                     {this.props.words.map(word => <Word key={word.id} word={word}/>)}
                 </ul>
-                <span className='words-list-score'>Total correct answers: {this.props.language.total_score}</span>
+                <span className='words-list-score'>Total correct answers: {totalCorrect}</span>
             </div>
         )
     }
