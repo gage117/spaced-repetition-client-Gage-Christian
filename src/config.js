91cf02dff1bb02007267d6b12e25074f567a2e1c
gage117 <57643501+gage117@users.noreply.github.com>
2020-05-19 14:24:10 -0500

    Fixed API endpoint

--- src/config.js
+++ src/config.js
@@ -1,6 +1,4 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://hidden-plateau-58997.herokuapp.com/api',
   TOKEN_KEY: '960f52b8-cd60-4fdb-8d93-30cdd2b3c32f',
 }
-
-// https://hidden-plateau-58997.herokuapp.com/api
